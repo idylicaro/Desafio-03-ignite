49276af4c27d3f76d3b6de140b24860372ebf770
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-08-14 13:48:31 -0300

    test: ensure org creation address and number

--- src/use-cases/create-org.spec.ts
+++ src/use-cases/create-org.spec.ts
@@ -1,58 +1,75 @@
-import { expect, describe, it, beforeEach } from 'vitest';
-import { CreateOrgUseCase } from './create-org';
-import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-reporitory';
-import { compare } from 'bcryptjs';
-import { OrgAlreadyExistsError } from './errors/org-already-exists-error';
+import { expect, describe, it, beforeEach } from 'vitest'
+import { CreateOrgUseCase } from './create-org'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-reporitory'
+import { compare } from 'bcryptjs'
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error'
 
-let orgsRepository: InMemoryOrgsRepository;
-let sut: CreateOrgUseCase;
+let orgsRepository: InMemoryOrgsRepository
+let sut: CreateOrgUseCase
 
 describe('Create Org Use Case', () => {
   beforeEach(() => {
-    orgsRepository = new InMemoryOrgsRepository();
-    sut = new CreateOrgUseCase(orgsRepository);
-  });
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new CreateOrgUseCase(orgsRepository)
+  })
 
   it('should create a new org', async () => {
     const { org } = await sut.execute({
       name: 'org1 name',
       email: 'org1@org.com',
-      password: '123456'
-    });
+      password: '123456',
+      address: 'org1 address',
+      address_number: 'org1 address_number',
+      city: 'org1 city',
+      state: 'org1 state',
+      phone: 'org1 phone',
+    })
 
-    expect(org.id).toEqual(expect.any(String));
-  });
+    expect(org.id).toEqual(expect.any(String))
+  })
 
   it('should hash org password upon creation', async () => {
     const { org } = await sut.execute({
       name: 'org1 name',
       email: 'org1@org.com',
-      password: '123456'
-    });
+      password: '123456',
+      address: 'org1 address',
+      address_number: 'org1 address_number',
+      city: 'org1 city',
+      state: 'org1 state',
+      phone: 'org1 phone',
+    })
 
-    const isPasswordCorrectlyHashed = await compare(
-      '123456',
-      org.password_hash
-    );
+    const isPasswordCorrectlyHashed = await compare('123456', org.password_hash)
 
-    expect(isPasswordCorrectlyHashed).toBe(true);
-  });
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 
   it('should not to be able to create a new org with the same email twice', async () => {
-    const email = 'org1@org.com';
+    const email = 'org1@org.com'
 
     await sut.execute({
       name: 'org1 name',
-      email: email,
-      password: '123456'
-    });
+      email,
+      password: '123456',
+      address: 'org1 address',
+      address_number: 'org1 address_number',
+      city: 'org1 city',
+      state: 'org1 state',
+      phone: 'org1 phone',
+    })
 
     await expect(() =>
       sut.execute({
         name: 'org2 name',
-        email: email,
-        password: '123456'
-      })
-    ).rejects.toBeInstanceOf(OrgAlreadyExistsError);
-  });
-});
+        email,
+        password: '123456',
+        address: 'org1 address',
+        address_number: 'org1 address_number',
+        city: 'org1 city',
+        state: 'org1 state',
+        phone: 'org1 phone',
+      }),
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
+  })
+})
