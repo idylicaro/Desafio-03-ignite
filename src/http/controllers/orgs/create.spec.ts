8c8d7ca11101fa2d5865114d074209ba3bf65285
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-08-15 20:59:44 -0300

    test: ensure org create new fields

--- src/http/controllers/orgs/create.spec.ts
+++ src/http/controllers/orgs/create.spec.ts
@@ -1,23 +1,28 @@
-import request from 'supertest';
-import { app } from '@/app';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Register (e2e)', () => {
   beforeAll(async () => {
-    await app.ready();
-  });
+    await app.ready()
+  })
 
   afterAll(async () => {
-    await app.close();
-  });
+    await app.close()
+  })
 
   it('should be able to create', async () => {
     const response = await request(app.server).post('/orgs').send({
       name: 'Pet org fundation',
       email: 'pet_org_fundation@org.com',
-      password: '123456'
-    });
+      password: '123456',
+      address: 'Rua dos bobos',
+      address_number: '5',
+      city: 'São Paulo',
+      state: 'SP',
+      phone: '11999999999',
+    })
 
-    expect(response.statusCode).toEqual(201);
-  });
-});
+    expect(response.statusCode).toEqual(201)
+  })
+})
