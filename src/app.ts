83ba758c4f2263e784533c64db464a64ea2db0fa
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-08-15 20:51:54 -0300

    feat: add authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,38 @@
-import fastify from 'fastify';
-import { ZodError } from 'zod';
-import { env } from '@/env';
-import { orgsRoutes } from './http/controllers/orgs/routes';
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from '@/env'
+import { orgsRoutes } from './http/controllers/orgs/routes'
 
-export const app = fastify();
+export const app = fastify()
 
-app.register(orgsRoutes);
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
+app.register(orgsRoutes)
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation error.', issues: error.format() });
+      .send({ message: 'Validation error.', issues: error.format() })
   }
 
   if (env.NODE_ENV !== 'production') {
-    console.error(error);
+    console.error(error)
   } else {
     // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
-  return reply.status(500).send({ message: 'Internal server error.' });
-});
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,7 +1,12 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance } from 'fastify'
 
-import { create } from './create';
+import { create } from './create'
+import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
-  app.post('/orgs', create);
+  app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
+  app.post('/orgs', create)
 }
