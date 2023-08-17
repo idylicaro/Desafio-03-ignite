7361969d027a156d2ca11390156546866fb603cd
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-08-16 22:25:19 -0300

    test: ensure profile use case

---
+++ src/use-cases/get-org-profile.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-reporitory'
+import { GetOrgProfileUseCase } from './get-org-profile'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { OrgNotFoundError } from './errors/org-not-found-error'
+
+let repository: InMemoryOrgsRepository
+let service: GetOrgProfileUseCase
+
+describe('Get Org Profile Service', () => {
+  beforeEach(() => {
+    repository = new InMemoryOrgsRepository()
+    service = new GetOrgProfileUseCase(repository)
+  })
+
+  it('should be able to get org profile', async () => {
+    const orgCreated = await repository.create({
+      name: 'org1 name',
+      email: 'org@mail.com',
+      password_hash: await hash('123456', 6),
+      address: 'org1 address',
+      address_number: 'org1 address_number',
+      city: 'org1 city',
+      state: 'org1 state',
+      phone: 'org1 phone',
+    })
+
+    const { org } = await service.execute({
+      orgId: orgCreated.id,
+    })
+
+    expect(org.id).toEqual(expect.any(String))
+    expect(org.name).toEqual('org1 name')
+  })
+
+  it('should not be able to get org profile with wrong id', async () => {
+    await expect(() =>
+      service.execute({
+        orgId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(OrgNotFoundError)
+  })
+})
