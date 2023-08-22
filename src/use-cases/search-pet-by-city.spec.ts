463174401f955186ade8471704f186e5f986ae16
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-08-21 22:26:26 -0300

    test: ensure be able to search pets by city

---
+++ src/use-cases/search-pet-by-city.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-reporitory'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pet-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { SearchPetsByCityUseCase } from './search-pet-by-city'
+
+let orgsRepository: InMemoryOrgsRepository
+let petsRepository: InMemoryPetsRepository
+let sut: SearchPetsByCityUseCase
+
+describe('Get Org Profile Service', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    petsRepository = new InMemoryPetsRepository()
+    sut = new SearchPetsByCityUseCase(orgsRepository, petsRepository)
+  })
+
+  it('should be able to search pets by city', async () => {
+    const org = await orgsRepository.create({
+      name: 'org1 name',
+      email: 'org1@mail.com',
+      city: 'org1 city',
+      state: 'org1 state',
+      address: 'org1 address',
+      address_number: 'org1 address_number',
+      password_hash: 'org1 password_hash',
+      phone: 'org1 phone',
+    })
+
+    const pet = await petsRepository.create({
+      name: 'pet1 name',
+      age: 1,
+      org_id: org.id,
+      height: 1,
+      weight: 1,
+    })
+
+    const { pets } = await sut.execute({
+      query: 'org1 city',
+    })
+
+    expect(pets).toEqual([pet])
+    expect(pets).toHaveLength(1)
+  })
+})
